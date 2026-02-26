307c0a35277672ab34c7b076b308b7fe7332cc75
thindr999 <thindr999@gmail.com>
2026-02-26 17:00:55 +0800

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{padding:"40px"}}>
+      <h1>HireWire</h1>
+      <p>Book trusted hospitality staff on demand</p>
+    </div>
+  );
+}
